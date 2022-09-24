0403aa728a54de90ccf908febf68e32683420fbd
Clementweb3 <coderfanai77@gmail.com>
2022-09-24 11:06:56 +0530

    change vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-      }
-    })
-  ]
+  plugins: [react()]
 })
